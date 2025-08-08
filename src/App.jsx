1524a039f443275e4eb6c27da6a3e0c1afc8efc1
Felipe Alves <felipealves1484@gmail.com>
2025-08-08 16:25:46 -0300

    :sparkles: feat: Adiocionada componente Certificates. :lipstick: feat: Adicionada estilização há Certificates.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from './assets/Components/Header/Header'
 import Hero from './assets/Components/Hero/Hero'
 import Projects from './assets/Components/Projects/Projects'
 import Knowledge from './assets/Components/Knowledge/Knowledge'
+import Certificates from './assets/Components/Certificates/Certificates'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
     <Header/>
     <Hero/>
     <Projects/>
+    <Certificates/>
     <Knowledge/>
     </>
   )
---
+++ src/assets/Components/Certificates/Certificates.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import './Certificates.css'
+
+
+import rocketseat from '../../images/Certificates/Rocketseat.png'
+
+function Certificates() {
+    const [showAllCertificates, setShowAllCertificates] = useState (false)
+
+    const certificates = [
+        {
+            img: rocketseat,
+            title: "Minicurso React Native",
+            conclusion: "Concluído em 17 de Julho de 2025",
+            link: "https://app.rocketseat.com.br/certificates/36b1e1d6-1363-4642-9bc2-844b729cabaa"
+        },
+        {
+            img: rocketseat,
+            title: "NLW #20",
+            conclusion: "Concluído em 11 de Julho de 2025",
+            link: "https://app.rocketseat.com.br/certificates/c92b616f-baf1-4025-8c3a-299908473108"
+        }
+    ]
+
+    const visibleCertificates = showAllCertificates ? certificates : certificates.slice(0, 3)
+
+    return(
+        <section id="certificates" className="projects-container">
+            <h1>Certificados</h1>
+            <div className="main-container">
+                <div className="projects-grid">
+                    {visibleCertificates.map((certificate, index) => (
+                        <div className="project-card" key={index}>
+                            <img src={certificate.img} alt={certificate.title} />
+                            <h2>{certificate.title}</h2>
+                            <p>{certificate.conclusion}</p>
+                            <a 
+                                href={certificate.link}
+                                className="github-link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
+                                    Ver Certificado
+                                </a>
+                        </div>
+                    ))}
+                </div>
+
+                {!showAllCertificates && certificates.length > 3 && (
+                    <div className="button-container">
+                        <button
+                        className="load-more-btn"
+                        onClick={() => setShowAllCertificates(true)}
+                        >
+                            Ver mais Certificados
+                        </button>
+                    </div>
+                )}
+            </div>
+        </section>
+    )
+}
+
+export default Certificates
