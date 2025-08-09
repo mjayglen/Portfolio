760baacf40fa10cffc9719a402c957c2cb8c70df
Felipe Alves <felipealves1484@gmail.com>
2025-08-09 16:13:59 -0300

    :books: docs: Atualizado Links de Footer

--- src/assets/Components/Footer/Footer.jsx
+++ src/assets/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ function Footer () {
                 <h1 className="title">Entre em contato</h1>
                 <p className="description">Como posso colaborar  com o seu projeto?</p>
                 <div className="social-links">
-                    <a href="https://github.com/felipeassisdev" target="_blank" title="Github"><SiGithub/></a>
+                    <a href="https://github.com/felipe-allves" target="_blank" title="Github"><SiGithub/></a>
                     <a href="https://instagram.com/itz._felipe1" target="_blank" title="Instagram"><SiInstagram/></a>
                     <a href="https://www.linkedin.com/in/felipe-alves-791a3a282/" target="_blank" title="Linkedin"><SiLinkedin/></a>
                     <a href="mailto:felipealves1484@gmail.com" target="_blank" title="Email"><SiGmail/></a>
